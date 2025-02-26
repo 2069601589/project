92fbd9d4294563583fc4e41cb23d7cfd4b1af29a
叶夜烨 <2069601589@qq.com>
2025-02-26 09:10:28 +0800

    user_manage_vue

---
+++ user-maneger-react/user_manager_back/src/test/java/com/example/user_manager_back/UserManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_manager_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
