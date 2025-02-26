87e127df9e5e764124c38a7fb221087c0b98545b
叶夜烨 <2069601589@qq.com>
2025-02-26 12:36:25 +0800

    后端demo成功运行

---
+++ user-maneger-react/user_manager_back/src/main/java/com/example/user_manager_back/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.user_manager_back.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.user_manager_back.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
