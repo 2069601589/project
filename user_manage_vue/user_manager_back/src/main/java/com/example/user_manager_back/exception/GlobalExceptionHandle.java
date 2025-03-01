2c7ff20e6c072adba41db10d18c90ba246f3c399
叶夜烨 <2069601589@qq.com>
2025-03-01 12:35:41 +0800

    异常全局处理

--- user_manage_vue/user_manager_back/src/main/java/com/example/user_manager_back/exception/GlobalExceptionHandle.java
+++ user_manage_vue/user_manager_back/src/main/java/com/example/user_manager_back/exception/GlobalExceptionHandle.java
@@ -2,14 +2,21 @@ package com.example.user_manager_back.exception;
 import com.example.user_manager_back.common.BaseResponse;
 import com.example.user_manager_back.common.ErrorCode;
 import com.example.user_manager_back.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandle {
+   @ExceptionHandler(BusinessException.class)
    public BaseResponse businessExceptionHandler(BusinessException e) {
+       log.error("业务异常: {}", e.getMessage(), e);
       return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
    }
+   @ExceptionHandler(RuntimeException.class)
    public BaseResponse runtimeExceptionHandler(RuntimeException e) {
+      log.error("运行时异常", e);
       return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
    }
 }
